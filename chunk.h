8c4d9e935e0af50bbe90f4c1f4df98e9d2c6a089
Ryan Martz <martzrp@gmail.com>
2020-09-13 08:20:41 -0400

    add struct chunk definition and init method

--- chunk.h
+++ chunk.h
@@ -7,4 +7,12 @@ typedef enum {
     OP_RETURN,
 } OpCode;
 
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code; // wrapper around array of bytes
+} Chunk; // this struct represents a dynamic array of bytes
+
+void initChunk(Chunk* chunk);
+
 #endif
