074e457f9c015ead6bef867c9d32a65167f1f96a
Ryan Martz <martzrp@gmail.com>
2020-09-14 08:03:51 -0400

    add line management logic when writing a chunk of bytecode

--- chunk.c
+++ chunk.c
@@ -19,15 +19,17 @@ void freeChunk(Chunk* chunk) {
 }
 
 // this function can write opcodes or operands - all just bytes
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     if(chunk->capacity < chunk->count + 1) {
         // need to resize array
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
