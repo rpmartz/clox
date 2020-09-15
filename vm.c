b239da901bc40482af2056e463d7e8bf1d960f3d
Ryan Martz <martzrp@gmail.com>
2020-09-14 21:54:02 -0400

    implement handling of constants in vm

--- vm.c
+++ vm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -11,8 +13,34 @@ void freeVM() {
 
 }
 
-InterpretResult interprest(Chunk* chunk) {
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++) // read the byte currently pointed at by the ip and increment it (increment before execution)
+
+// read next byte in bytecode, which is index of constant in const pool, and look it up
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for(;;) { // every turn through this loop, we will read and execute a single bytecode instruction
+        uint8_t instruction;
+        switch(instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
     vm.chunk = chunk; // store chunk being executed in vm
     vm.ip = vm.chunk->code; // point to first byte of code in the chunk
-    return run()l
+    return run();
 }
