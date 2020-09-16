41f5cb0926d57a84762928dcf2f62475124e93a2
Ryan Martz <martzrp@gmail.com>
2020-09-15 21:40:16 -0400

    add negation to bytecode

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main() {
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 341);
     writeChunk(&chunk, constant, 341);
+    writeChunk(&chunk, OP_NEGATE, 341);
 
     writeChunk(&chunk, OP_RETURN, 341);
     disassembleChunk(&chunk, "test chunk");
