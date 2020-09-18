77f45b0db2f9a1fc99c5fc8137c84dde2a41b1b7
Ryan Martz <martzrp@gmail.com>
2020-09-17 20:45:32 -0400

    add initial scanner functions

--- compiler.c
+++ compiler.c
@@ -7,4 +7,25 @@
 void compile(const char* source) {
     // first phase of compilation is scanning
     initScanner(source);
+
+    // temporary code for driving the scanner
+    int line = -1;
+    for(;;) {
+        Token token = scanToken();
+        if(token.line != line) {
+            // print line number
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+
+        // print numeric token type and the token
+        // %.*s allows variable length tokens
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) {
+            break;
+        }
+    }
 }
